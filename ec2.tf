e05d244e388d9816dba66b7eaa1358fc5f278cf1
Raghupathy <ragupathi.kommidi@gmail.com>
2022-09-01 07:05:27 +0530

    yes

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,24 @@
 #Create security group with firewall rules
+
+resource "aws_vpc" "vpc" {
+   tags = {
+    Name = "Second VPC"
+  }
+  cidr_block = "10.0.0.0/20"
+}
+
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+   tags = {
+    Name = "Main"
+  }
+}
 resource "aws_security_group" "my_security_group" {
   name        = "default-1"
   description = "security group for Ec2 instance"
+  vpc_id      = aws_vpc.vpc.id
 
   ingress {
     from_port   = 8080
@@ -30,28 +47,12 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-
-resource "aws_vpc" "vpc" {
-   tags = {
-    Name = "Second VPC"
-  }
-  cidr_block = "10.0.0.0/20"
-}
-
-resource "aws_subnet" "name" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
-
-   tags = {
-    Name = "Main"
-  }
-}
 resource "aws_instance" "terraform_wapp" {
   # for_each = ["ssds", "sfsfsfsdfsd", "S"]
   ami                         = var.ami
   instance_type               = var.instance_type
   # vpc_security_group_ids      = ["${data.aws_security_group.selected.id}"]
-  vpc_security_group_ids      = ["sg-089c40d4eedaecd72"]
+  vpc_security_group_ids      = [aws_security_group.my_security_group.id]
   subnet_id                   = aws_subnet.name.id
   # count                       = 2
   associate_public_ip_address = true
